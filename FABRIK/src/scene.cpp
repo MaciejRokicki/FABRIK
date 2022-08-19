c791782a7122e282d9657f960e442323deecb9af
Maciej Rokicki <rokicki.mk@gmail.com>
2022-08-19 10:08:26 +0200

    3d constraints demo

--- FABRIK/src/scene.cpp
+++ FABRIK/src/scene.cpp
@@ -387,9 +387,10 @@ Scene* Scene::BuildScene5() {
 
     Node<Joint3D>* root = new Node<Joint3D>(Joint3D(Vector3::zero, Vector3::one / 2, { 0.5f, 0.0f, 1.0f, 1.0f }));
 
-    root->next(                     Joint3D(Vector3{ 0.5f, 1.50f, 0.0f }, Vector3::one / 3, { 1.0f, 0.0f, 0.0f, 1.0f }, new Twist3D(10.0f, 170.0f, 0.0f, 360.0f)));
-    root->child[0]->next(           Joint3D(Vector3{ 1.5f, 1.75f, 0.0f }, Vector3::one / 3, { 1.0f, 0.0f, 0.0f, 1.0f }, new Hinge3D(Axis::Y, 0.0f, 90.0f)));
-    root->child[0]->child[0]->next( Joint3D(Vector3{ 2.0f, 1.50f, 0.0f }, Vector3::one / 3, { 1.0f, 0.0f, 0.0f, 1.0f }, new Hinge3D(Axis::Y, 0.0f, 90.0f)));
+    root->next(                               Joint3D(Vector3{ 0.50f, 1.50f, 0.0f }, Vector3::one / 3, { 1.0f, 0.0f, 0.0f, 1.0f }, new Twist3D(10.0f, 170.0f, 0.0f, 360.0f)));
+    root->child[0]->next(                     Joint3D(Vector3{ 1.50f, 1.75f, 0.0f }, Vector3::one / 3, { 1.0f, 0.0f, 0.0f, 1.0f }, new Hinge3D(Axis::X, 190.0f, 0.0f)));
+    root->child[0]->child[0]->next(           Joint3D(Vector3{ 2.00f, 1.50f, 0.0f }, Vector3::one / 3, { 1.0f, 0.0f, 0.0f, 1.0f }, new Hinge3D(Axis::Y, 190.0f, 0.0f)));
+    root->child[0]->child[0]->child[0]->next( Joint3D(Vector3{ 2.25f, 2.00f, 0.0f }, Vector3::one / 3, { 1.0f, 0.0f, 0.0f, 1.0f }));
 
     Tree<Joint3D>* tree = new Tree<Joint3D>(root);
     std::vector<Target3D*>* targets = new std::vector<Target3D*>();
